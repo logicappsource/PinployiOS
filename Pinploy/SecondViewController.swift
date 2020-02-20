9ab6a740a9d1b9d30399d8acfdca46506a170697
logicappsource <logicappsource@gmail.com>
2020-02-20 19:10:59 +0100

    Initial Commit

---
+++ Pinploy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pinploy
+//
+//  Created by dev on 20/02/2020.
+//  Copyright © 2020 logicappsource. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
